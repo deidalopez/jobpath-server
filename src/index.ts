fcbcbf5a77ab95ec20fa7665a4906d89b80cc570
Deida <deidalopez1@gmail.com>
2021-04-18 12:36:56 -0700

    Include Jest testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express";
+
+const app = express();
+
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
